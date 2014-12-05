65f337b1296a5ea7cbdb7415a020649e56635c39
hikari-NYU <wm724@nyu.edu>
2014-12-05 17:02:06 -0500

    Test

--- src/clustering/EntityWrapper.java
+++ src/clustering/EntityWrapper.java
@@ -5,62 +5,37 @@ import java.util.List;
 import org.apache.commons.math3.ml.clustering.Clusterable;
 
 public class EntityWrapper implements Clusterable {
+	private int corpusID;
+	private String user;
 	private double[] position;
 	private static AttributeDict dict;
-	private String name;
-	private Entity entity;
 	
-	public EntityWrapper(Entity entity, String dictPath) throws Exception {
-		this.entity=entity;
-		dict=new AttributeDict();
-		if(null != dictPath) {
-			dict.load(dictPath);
+	public EntityWrapper(String user, String tweet, String dictPath, int dimension) 
+			throws Exception {
+		this.user=user;
+		position=new double[dimension];
+		for(int i=0;i<dimension;i++) {
+			position[i]=0;
 		}
-		this.name = entity.entityName;
-		this.position=new double[entity.attribute.length];
-		for(int i=0;i<entity.attribute.length;i++) {
-			if(entity.attribute[i] == null) {
-				position[i] = -1;
-			}
-			else {
-				position[i]=attrAbstract(entity.attribute[i]);
+		tweet = tweet.replaceAll(".,!?", " ");
+		String[] tokens=tweet.split("\\s+");
+		for(int i=0;i<tokens.length;i++) {
+			int ind=dict.search(tokens[i]);
+			if(ind != -1) {
+				position[ind]++;
 			}
 		}
 	}
 	
-	private double attrAbstract(String attr) throws Exception {
-		double val=0.0;
-		int score=0;
-		attr=attr.replaceAll("[^a-zA-Z0-9]*", "");
-		score=dict.search(attr);
-		val=(double)score;
-		return val;
-	}
-	
-	public Entity getEntities() {
-		return this.entity;
+	public String getUser() {
+		return this.user;
 	}
 	@Override
 	public double[] getPoint() {
 		return this.position;
 	}
 	
-	public String getName() {
-		return name;
-	}
-	
 	public AttributeDict getDict() {
 		return dict;
-	}
-	
-	public double[] getPosArr() {
-		return position;
-	}
-	
-	public void setPosArr(double[] position) {
-		this.position=new double[position.length];
-		for(int i=0;i<this.position.length;i++) {
-			this.position[i]=position[i];
-		}
 	}
 }
