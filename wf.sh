dbb5c4edd7da975db017ea1a1dc54f7491648343
bbccyy <bbccyy@users.noreply.github.com>
2014-12-04 19:02:44 -0500

    Create wf.sh
    chmod +x ./myfirstsh.sh

---
+++ wf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'World Filter for passed ouputs'
+
+node extractEnWords.js
+
+cat ./output.txt | tr -cs A-Za-z\' '\n' | tr A-Z a-z | sort | uniq -c | sort -k1,1nr -k2 | sed ${1:-300}q >./wordsList.txt
